88292de240c984f777f92dc8869041aeec7f8463
tomotsuka.masaki <tomotsuka.masaki@pocke.co.jp>
2021-05-07 18:20:44 +0900

    vue component replace tabs to 2spaces

--- src/components/LifeGame.vue
+++ src/components/LifeGame.vue
@@ -1,25 +1,25 @@
 <template>
-		<div class="lifegame">
-			<div class="board">
-				<div 
-					v-for="(row, i) in board"
-					:key="i"
-					class="row"
-				>
-					<Cell
-						v-for="cell in row"
-						:key="cell.cellId"
-						:live="cell.live"
-					/>
-				</div>
-			</div>
+    <div class="lifegame">
+      <div class="board">
+        <div 
+          v-for="(row, i) in board"
+          :key="i"
+          class="row"
+        >
+          <Cell
+            v-for="cell in row"
+            :key="cell.cellId"
+            :live="cell.live"
+          />
+        </div>
+      </div>
 
-			<div class="buttons">
-				<button @click="start">start</button>
-				<button @click="stop">stop</button>
-				<button @click="reset">reset</button>
-			</div>
-		</div>
+      <div class="buttons">
+        <button @click="start">start</button>
+        <button @click="stop">stop</button>
+        <button @click="reset">reset</button>
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -28,33 +28,33 @@ import Cell from './Cell.vue'
 import { useBoard } from '../hooks/useBoard'
 
 export default defineComponent({
-	components: {
-		Cell
-	},
-	setup() {
-		const { board, init, reset, start } = useBoard({ cells: 32 })
+  components: {
+    Cell
+  },
+  setup() {
+    const { board, init, reset, start } = useBoard({ cells: 32 })
 
-		onMounted(() => {
-			init()
-		})
+    onMounted(() => {
+      init()
+    })
 
-		return {
-			board,
-			reset,
-			start,
-			stop
-		}
-	},
+    return {
+      board,
+      reset,
+      start,
+      stop
+    }
+  },
 })
 </script>
 
 <style scoped>
 .row {
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .buttons {
-	text-align: center;
+  text-align: center;
 }
 </style>
